6de5f511560c84c53637922b2d0cc0c3836ac350
Richard Fenix <richard.fenix@gmail.com>
2018-07-23 04:48:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
-        grade INTEGER
+        grade TEXT
         )
         SQL
     DB[:conn].execute(sql) 
@@ -34,5 +34,11 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
+    it 'saves an instance of the Student class to the database' do
+      Student.create_table
+      josh.save
+      expect(josh.id).to eq(1)
+      expect(DB[:conn].execute("SELECT * FROM students")).to eq([[1, "Josh", "9th"]])
+    end
 
 end
