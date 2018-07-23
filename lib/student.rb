bc0df1fedb723efe34efef33b5407154bff37fdf
Richard Fenix <richard.fenix@gmail.com>
2018-07-23 04:58:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,9 +43,4 @@ class Student
     # binding.pry
   end
   
-  # it 'takes in a hash of attributes and uses metaprogramming to create a new student object. Then it uses the #save method to save that student to the database' do
-  #     Student.create(name: "Sally", grade: "10th")
-  #     expect(DB[:conn].execute("SELECT * FROM students")).to eq([[1, "Sally", "10th"]])
-  #   end
-
 end
