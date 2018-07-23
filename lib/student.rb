ba6c85df55b78ebf298a1ed54f2f9b452ca23c5d
Richard Fenix <richard.fenix@gmail.com>
2018-07-23 04:38:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,9 +24,7 @@ class Student
   end
   
   def self.drop_table
-    sql =  <<-SQL 
-      DROP TABLE IF EXISTS students
-        SQL
+    sql =  "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql) 
   end
 
