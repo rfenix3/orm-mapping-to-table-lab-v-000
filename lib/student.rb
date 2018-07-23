c7d12645cd025e03ff47b650089444891bba1c2c
Richard Fenix <richard.fenix@gmail.com>
2018-07-23 04:31:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,14 +11,5 @@ class Student
     @grade = grade
     @id = id
   end
-      it 'has a name and a grade' do
-      student = Student.new("Tiffany", "11th")
-      expect(student.name).to eq("Tiffany")
-      expect(student.grade).to eq("11th")
-    end
-
-    it 'has an id that is readable but not writable' do
-      expect{josh.id = 1}.to raise_error(NoMethodError)
-    end
 
 end
