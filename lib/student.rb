a46a008f4fbd6ea83acf1b2f5c13f3182eee44dc
Richard Fenix <richard.fenix@gmail.com>
2018-07-23 04:57:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
     student = Student.new(name, grade)
     student.save
     student
-    binding.pry
+    # binding.pry
   end
   
   # it 'takes in a hash of attributes and uses metaprogramming to create a new student object. Then it uses the #save method to save that student to the database' do
